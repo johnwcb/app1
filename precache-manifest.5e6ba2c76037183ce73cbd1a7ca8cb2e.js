642b32a9028232be72ed8aed504bcef00dffd65b
john <johnwcb@gmail.com>
2020-03-04 22:51:52 -0800

    Updates

---
+++ precache-manifest.5e6ba2c76037183ce73cbd1a7ca8cb2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da826f82f1d720a4c1169cbb2f915985",
+    "url": "/app1/index.html"
+  },
+  {
+    "revision": "9258b4032a1bbcea1bee",
+    "url": "/app1/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "80031c6bc60b7d8592ce",
+    "url": "/app1/static/js/2.ee222a62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/app1/static/js/2.ee222a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9258b4032a1bbcea1bee",
+    "url": "/app1/static/js/main.92f0f8a4.chunk.js"
+  },
+  {
+    "revision": "14c4fb8e45ac8df3bb12",
+    "url": "/app1/static/js/runtime-main.cba09ce6.js"
+  }
+]);
